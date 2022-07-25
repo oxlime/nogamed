c8dd25e3fde0cf3a7d3d259be622009a5f974d6f
oxlime <sean.gne.19@gmail.com>
2022-07-25 17:30:19 -0500

    clean main

--- main.go
+++ main.go
@@ -5,9 +5,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
-	"math/big"
 	"os"
-	"strconv"
 	"strings"
 	"time"
 
@@ -30,201 +28,12 @@ var (
 	pollInterval int    = 150
 )
 
-func ogamecall(gw *gateway.GatewayProvider, eps string) ([]string, error) {
-	resp, err := gw.Call(context.Background(), types.FunctionCall{
-		ContractAddress:    nogameContract,
-		EntryPointSelector: eps,
-		Calldata: []string{
-			addressDec,
-		},
-	}, "")
-	if err != nil {
-		return nil, err
-	}
-	return resp, nil
-}
-
-func collectResources(gw *gateway.GatewayProvider, account *caigo.Account) {
-	collect := []types.Transaction{
-		{
-			ContractAddress:    nogameContract,
-			EntryPointSelector: "collect_resources",
-		},
-	}
-
-	// estimate fee for executing transaction
-	feeEstimate, err := account.EstimateFee(context.Background(), collect)
-	if err != nil {
-		panic(err.Error())
-	}
-	fee := types.Felt{
-		Int: new(big.Int).SetUint64((feeEstimate.OverallFee * feeMargin) / 100),
-	}
-	fmt.Printf("Fee:\n\tEstimate\t\t%v wei\n\tEstimate+Margin\t\t%v wei\n\n", feeEstimate.OverallFee, fee)
-
-	// execute transaction
-	execResp, err := account.Execute(context.Background(), &fee, collect)
-	if err != nil {
-		panic(err.Error())
-	}
-	fmt.Println("execution response: ", execResp.TransactionHash)
-
-	n, receipt, err := gw.PollTx(context.Background(), execResp.TransactionHash, types.ACCEPTED_ON_L2, pollInterval, maxPoll)
-	if err != nil {
-		panic(err.Error())
-	}
-	fmt.Printf("Poll %dsec %dx \n\ttransaction(%s) receipt: %s\n\n", n*pollInterval, n, execResp.TransactionHash, receipt.Status)
-}
-
-func upgradeMine(gw *gateway.GatewayProvider, eps string, account *caigo.Account) error {
-	increment := []types.Transaction{
-		{
-			ContractAddress:    nogameContract,
-			EntryPointSelector: eps + "_upgrade_start",
-		},
-	}
-
-	// estimate fee for executing transaction
-	feeEstimate, err := account.EstimateFee(context.Background(), increment)
-	if err != nil {
-		if strings.Contains(err.Error(), "Error message: ERC20: burn amount exceeds balance") {
-			time.Sleep(10 * time.Second)
-			fmt.Println("caught error")
-			return err
-		}
-		panic(err.Error())
-	}
-	fee := types.Felt{
-		Int: new(big.Int).SetUint64((feeEstimate.OverallFee * feeMargin) / 100),
-	}
-	fmt.Printf("Fee:\n\tEstimate\t\t%v wei\n\tEstimate+Margin\t\t%v wei\n\n", feeEstimate.OverallFee, fee)
-
-	// execute transaction
-	execResp, err := account.Execute(context.Background(), &fee, increment)
-	if err != nil {
-		panic(err.Error())
-	}
-	fmt.Println("execution response: ", execResp.TransactionHash)
-
-	n, receipt, err := gw.PollTx(context.Background(), execResp.TransactionHash, types.ACCEPTED_ON_L2, pollInterval, maxPoll)
-	if err != nil {
-		panic(err.Error())
-	}
-	fmt.Printf("Poll %dsec %dx \n\ttransaction(%s) receipt: %s\n\n", n*pollInterval, n, execResp.TransactionHash, receipt.Status)
-
-	buildingId, buildTime, err := getBuildTimeCompletion(gw)
-	if err != nil {
-		panic(err.Error())
-	}
-	fmt.Println("building  id: ", buildingId)
-	fmt.Println("build time completion: ", buildTime)
-	completeMineUpgrade(gw, eps, account)
-	return nil
-}
-
-func completeMineUpgrade(gw *gateway.GatewayProvider, eps string, account *caigo.Account) {
-	complete := []types.Transaction{
-		{
-			ContractAddress:    nogameContract,
-			EntryPointSelector: eps + "_upgrade_complete",
-		},
-	}
-
-	// estimate fee for executing transaction
-	feeEstimate, err := account.EstimateFee(context.Background(), complete)
-	if err != nil {
-		panic(err.Error())
-	}
-	fee := types.Felt{
-		Int: new(big.Int).SetUint64((feeEstimate.OverallFee * feeMargin) / 100),
-	}
-	fmt.Printf("Fee:\n\tEstimate\t\t%v wei\n\tEstimate+Margin\t\t%v wei\n\n", feeEstimate.OverallFee, fee)
-
-	// execute transaction
-	execResp, err := account.Execute(context.Background(), &fee, complete)
-	if err != nil {
-		panic(err.Error())
-	}
-	fmt.Println("execution response: ", execResp.TransactionHash)
-
-	n, receipt, err := gw.PollTx(context.Background(), execResp.TransactionHash, types.ACCEPTED_ON_L2, pollInterval, maxPoll)
-	if err != nil {
-		panic(err.Error())
-	}
-	fmt.Printf("Poll %dsec %dx \n\ttransaction(%s) receipt: %s\n\n", n*pollInterval, n, execResp.TransactionHash, receipt.Status)
-}
-
-func getMineLevels(gw *gateway.GatewayProvider) (int64, int64, int64, int64, int64, error) {
-	resp, err := ogamecall(gw, "get_structures_levels")
-	if err != nil {
-		return 0, 0, 0, 0, 0, err
-	}
-	strmetal := resp[0]
-	metal, err := strconv.ParseInt(strmetal[2:], 16, 64)
-	if err != nil {
-		panic(err)
-	}
-	strcrystal := resp[1]
-	crystal, err := strconv.ParseInt(strcrystal[2:], 16, 64)
-	if err != nil {
-		panic(err)
-	}
-	strdeuterium := resp[2]
-	deuterium, err := strconv.ParseInt(strdeuterium[2:], 16, 64)
-	if err != nil {
-		panic(err)
-	}
-	strsolar := resp[3]
-	solar, err := strconv.ParseInt(strsolar[2:], 16, 64)
-	if err != nil {
-		panic(err)
-	}
-	strrobot := resp[4]
-	robot, err := strconv.ParseInt(strrobot[2:], 16, 64)
-	if err != nil {
-		panic(err)
-	}
-	return metal, crystal, deuterium, solar, robot, nil
-}
-
-func getBuildTimeCompletion(gw *gateway.GatewayProvider) (*types.Felt, *types.Felt, error) {
-	resp, err := ogamecall(gw, "build_time_completion")
-	if err != nil {
-		return nil, nil, err
-	}
-	buildingId := types.StrToFelt(resp[0])
-	timeEnd := types.StrToFelt(resp[1])
-	//test
-	respStr := resp[1]
-	respStr = respStr[2:]
-	timeInt, err := strconv.ParseInt(respStr, 16, 64)
-	if err != nil {
-		panic(err)
-	}
-	wTime(timeInt)
-	return buildingId, timeEnd, nil
-}
-
-//test
-func wTime(wait int64) {
-	fmt.Println("wait and time in unix", wait, time.Now().Unix())
-	sleepTime := wait - time.Now().Unix() + 265 //adding 65 seconds buffer
-	time.Sleep(time.Duration(sleepTime * 1000000000))
-	fmt.Println("sleep time = ", sleepTime)
-}
-
 func main() {
 	//leaderboard()
 	startMine()
 }
 
 func startMine() {
-	// init the stark curve with constants
-	// 'WithConstants()' will pull the StarkNet 'pedersen_params.json' file if you don't have it locally
-	//curve, err := caigo.SC(caigo.WithConstants())
-	//if err != nil {
-	//		panic(err.Error())
-	//	}
 
 	// init starknet gateway client
 	gw := gateway.NewProvider(gateway.WithChain(name))
@@ -425,8 +234,4 @@ func leaderboard() {
 	for index, element := range respoints {
 		fmt.Println("Wallet: ", resp[index], " Score: ", types.StrToFelt(element))
 	}
-
-	//mines := getResources(gw)
-	//fmt.Println("Resources on id 2: ", mines[2])
-
 }
